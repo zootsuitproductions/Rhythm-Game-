206e522ea4140a19e052d1cb1eef2754206aa7a5
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2020-04-02 15:26:37 -0700

    trying to make something happen when the level is completed

--- tyler/gameFunctionality.js
+++ tyler/gameFunctionality.js
@@ -1,6 +1,5 @@
 let fr = 60;
 let apple;
-let ang;
 let img;
 let bc = 100;
 let completed = false;
@@ -25,13 +24,8 @@ function preload(){
 
 function setup() {
 	createCanvas(700,400)
-	angle = 0;
-	apple = {
-		x: width/2,
-		y: 0,
-		width: 25,
-		height: 25
-	}
+	apple = new Fruit(width/2,25,25,0);
+	apple2 = new Fruit(50,25,25,90);
 	angleMode(DEGREES)
 }
 
@@ -41,11 +35,14 @@ function draw() {
 	fill(100)
 	textSize(50)
 	text('key is down', 0, 40)
-	angle += 2.5;
-	apple.y = map(sin(angle),-1,1,(height/2) + 20,(height/2)-20)
-	imageMode(CENTER)
+
 	if(!completed){
-		image(img,apple.x,apple.y,apple.width,apple.height)
+		apple.show()
+		apple2.show()
+	}
+	if(completed){
+		apple.boom()
+		apple2.boom()
 	}
 
 	if ((currentKey > -1) && (currentKey < 5)) {
@@ -91,3 +88,70 @@ function checkCorrectness() {
 	}
 	return correct;
 }
+
+class Fruit {
+	constructor(x,width,height,angle){
+		this.x = x;
+		this.y;
+		this.width = width;
+		this.height = height;
+		this.angle = angle;
+		this.array = [];
+		for (var i = 0; i < 30; i++) {
+			this.array.push(Math.floor(Math.random()*359))
+		}
+	}
+	show(){
+		this.angle += 2.5;
+		this.y = map(sin(this.angle),-1,1,(height/2) + 20,(height/2)-20)
+		imageMode(CENTER)
+		image(img,this.x,this.y,this.width,this.height)
+	}
+	boom(){
+		// let applePoof = new Bang(this.x,this.y)
+		// applePoof.pow()
+
+		// this.x += cos(this.d);
+		// this.y += sin(this.d);
+		// fill('red')
+		// ellipse(this.x,this.y,3,3)
+
+		for (var i = 0; i < this.array.length; i++) {
+			// this.bang(this.x,this.y, this.array[i])
+			let bingX = this.x;
+			let bingY = this.y;
+			bingX += cos(this.array[i]);
+			bingY += sin(this.array[i]);
+			fill('red')
+			ellipse(bingX,bingY,3,3)
+		}
+	}
+	bang(bangx,bangy,bangAngle){
+		bangx += cos(bangAngle);
+		bangy += sin(bangAngle);
+		for (var i = 0; i < 10; i++) {
+			fill('red')
+			ellipse(bangx,bangy,3,3)
+		}
+	}
+}
+
+// class Bang {
+// 	constructor(x,y){
+// 		this.x = x;
+// 		this.y = y;
+		// this.array = [];
+		// for (var i = 0; i < 30; i++) {
+		// 	this.array.push(Math.floor(Math.random()*359))
+		// }
+// 	}
+// 	pow(){
+// 		for (var i = 0; i < this.array.length; i++) {
+// 			fill('red')
+// 			ellipse(this.x,this.y,3,3)
+// 			this.x += cos(this.array[i]) * 10;
+// 			this.y += sin(this.array[i]) * 10;
+// 		}
+// 		console.log(this.array)
+// 	}
+// }
