57f4a7fa1a3496a005919a82db3a7789084d2492
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2020-04-30 14:26:31 -0700

    fixed

--- tyler/gameFunctionality.js
+++ tyler/gameFunctionality.js
@@ -1,4 +1,4 @@
-//55 doesnt change 33 because the explosion is already made
+//have explosion speed (might need to change the .move function)
 let fr = 60;
 let bc = 100;
 let apple;
@@ -9,6 +9,7 @@ let gameFruits = [];
 let currentKey = 0;
 let sequence = [];
 let explosions = [];
+let score = "terrible"
 
 document.addEventListener('keydown', function(){
 	bc = 50;
@@ -30,6 +31,7 @@ function setup() {
 		gameFruits.push(new Fruit((i * (width - 300) / (sequence.length - 1)) + 150 , 25, 25, 20 * i, sequence[i]))
 	}
 	angleMode(DEGREES)
+	textAlign(CENTER)
 }
 
 function draw() {
@@ -37,11 +39,7 @@ function draw() {
 	noStroke()
 	fill(100)
 	textSize(50)
-	text('key is down', 0, 40)
-	// for (var i = 0; i < explosions.length; i++) {
-	// 	explosions[i].move()
-	// 	console.log(explosions[i].x)
-	// }
+	text('key is down', 140, 40)
 
 	if(!completed){
 		for (var i = 0; i < gameFruits.length; i++) {
@@ -49,14 +47,13 @@ function draw() {
 		}
 	}
 	if(completed){
-		// for (var i = 0; i < gameFruits.length; i++) {
-		// 	explosions[i].x = gameFruits[i].x;
-		// 	explosions[i].y = gameFruits[i].y;
-		// }
 		for (var i = 0; i < explosions.length; i++) {
 			explosions[i].move()
 			// console.log(explosions[i].x)
 		}
+
+		bc = 50;
+		text("game over\n" + score,width/2,height/2)
 	}
 }
 
@@ -67,7 +64,9 @@ function keyPressed() {
 	}
 	if (currentKey == 5){
 		for (var i = 0; i < gameFruits.length; i++) {
-			explosions.push(new Explosion(gameFruits[i].x,gameFruits[i].y,gameFruits[i].array));
+			for (var j = 0; j < 30; j++) {
+				explosions.push(new Explosion(gameFruits[i].x,gameFruits[i].y,Math.floor(Math.random()*359)));
+			}
 		}
 	}
 }
@@ -79,11 +78,6 @@ class Fruit {
 		this.height = height;
 		this.angle = angle;
 		this.type = type;
-
-		this.array = [];
-		for (var i = 0; i < 30; i++) {
-			this.array.push(Math.floor(Math.random()*359))
-		}
 	}
 	show(){
 		this.angle += 2.5;
@@ -94,26 +88,15 @@ class Fruit {
 }
 
 class Explosion {
-	constructor(x,y,array){
+	constructor(x,y,direction){
 		this.x = x;
 		this.y = y;
-		this.array = array;
-
-		for (var i = 0; i < this.array.length; i++) {
-			this.explodeX = this.x;
-			this.explodeY = this.y;
-		}
+		this.direction = direction;
 	}
 	move(){
-		for (var i = 0; i < this.array.length; i++) {
-			// this.explodeX = this.x;
-			// this.explodeY = this.y;
-			fill('red')
-			ellipse(this.explodeX,this.explodeY,2,2)
-			this.explodeX+=cos(this.array[i]);
-			this.explodeY+=sin(this.array[i]);
-		}
-
+		fill('red')
 		ellipse(this.x,this.y,2,2)
+		this.x+=cos(this.direction);
+		this.y+=sin(this.direction);
 	}
 }
